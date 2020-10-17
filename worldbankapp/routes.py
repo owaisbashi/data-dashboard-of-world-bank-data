7f4987566821b00a87a5dd1df8bb82a7cfe7e466
Ovais Bashir <owaisbashir26@gmail.com>
2020-10-17 11:36:05 +0500

    flask plus pandas

--- worldbankapp/routes.py
+++ worldbankapp/routes.py
@@ -1,8 +1,14 @@
 from worldbankapp import app
 
 from flask import render_template
+from wrangling_scripts.wrangling import data_wrangling
+data = data_wrangling()
+
+print(data)
+
 
 @app.route('/')
 @app.route('/index')
+
 def index():
     return render_template('index.html')
